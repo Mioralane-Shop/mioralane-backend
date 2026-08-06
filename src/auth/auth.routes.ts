2aec99df2ec40da5607343e16dad063cc229086c
sabbir-salman <sabbir24salman@gmail.com>
2026-08-07 02:04:21 +0600

    test

--- src/auth/auth.routes.ts
+++ src/auth/auth.routes.ts
@@ -3,7 +3,6 @@ import { registerUser, loginUser, logoutUser } from './auth.controller';
 
 const router = Router();
 
-//test
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/logout', logoutUser);
